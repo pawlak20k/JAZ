6643ad0ad5390e75a03bfbcf0c3b2e287834ab7a
Paul20K <115339814+pawlak20k@users.noreply.github.com>
2024-06-29 12:07:17 +0200

    Delete pawkle/src/main/java/pl/pjatk/pawkle/Repository.java

--- pawkle/src/main/java/pl/pjatk/pawkle/Repository.java
+++
@@ -1,7 +0,0 @@
-package pl.pjatk.pawkle;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.pjatk.pawkle.model.Nbp;
-
-public interface Repository extends JpaRepository<Nbp, Long> {
-}
